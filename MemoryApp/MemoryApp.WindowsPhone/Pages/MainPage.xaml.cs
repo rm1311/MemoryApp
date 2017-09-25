2c9ccf288f5bb0153a965fdd55eb633582fabca4
rm <rm@ROBERT>
2017-09-25 21:27:29 +0200

    new pages added

---
+++ MemoryApp/MemoryApp.WindowsPhone/Pages/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ConsoleApplication1.Classes;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using MemoryApp.Pages;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MemoryApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            MemTable memttable = new MemTable();
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(NewGame));
+        }
+
+        private void Scores_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Scores));
+
+        }
+
+        private void Rules_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Rules));
+        }
+
+        private void Author_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Author));
+        }
+    }
+}
--- MemoryApp/MemoryApp.WindowsPhone/Pages/Scores.xaml.cs
+++ MemoryApp/MemoryApp.WindowsPhone/Pages/Scores.xaml.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleApplication1.Classes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace MemoryApp
+namespace MemoryApp.Pages
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Scores : Page
     {
-        public MainPage()
+        public Scores()
         {
-            MemTable memttable = new MemTable();
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-            
         }
 
         /// <summary>
@@ -40,13 +34,6 @@ namespace MemoryApp
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
         }
     }
 }
